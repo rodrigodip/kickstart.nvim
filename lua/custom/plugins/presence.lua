116cb055e0ceba52c0f27baf8b7bbbce5dc038bd
rodrigodip <rodrigodip@gmail.com>
2025-06-04 08:50:15 -0300

    update init.lua + plugins

---
+++ lua/custom/plugins/presence.lua
@@ -0,0 +1,10 @@
+-- presence.lua
+return {
+  'andweeb/presence.nvim',
+  opts = {
+    show_time = false,
+    log_level = 'warn', -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error" or nil)
+    workspace_text = 'Working on %s',
+    -- Etc..
+  },
+}
